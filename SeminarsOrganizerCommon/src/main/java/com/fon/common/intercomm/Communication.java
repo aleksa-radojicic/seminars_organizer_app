f5df07497fa037213fa97d5a2ea30a234c21bcf4
aleksa-radojicic <aleksa.radojicic@gmail.com>
2023-11-28 23:15:51 +0100

    Added javadoc to classes in intercomm package

--- SeminarsOrganizerCommon/src/main/java/com/fon/common/intercomm/Communication.java
+++ SeminarsOrganizerCommon/src/main/java/com/fon/common/intercomm/Communication.java
@@ -10,24 +10,64 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
+ * Singleton class that manages communication functionalities via sockets.
+ *
+ * <p>
+ * This class provides methods to handle sending and receiving objects through
+ * sockets.
+ * </p>
  *
  * @author Aleksa
+ * @since 0.0.1
+ *
  */
 public class Communication {
+
+    /**
+     * The singleton instance of the Communication class.
+     */
     private static Communication instance;
 
+    /**
+     * Getter of instance (Singleton pattern).
+     *
+     * @return The singleton instance of the Communication class.
+     */
     public static Communication getInstance() {
-        if (instance == null)
+        if (instance == null) {
             instance = new Communication();
+        }
         return instance;
     }
-    
+
+    /**
+     * Private non-parametric constructor.
+     */
+    private Communication() {
+    }
+
+    /**
+     * Sends an object through the provided socket's output stream.
+     *
+     * @param socket The socket used for communication.
+     * @param object The object to be sent.
+     * @throws IOException If an I/O error occurs during the sending process.
+     */
     public void send(Socket socket, Object object) throws IOException {
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(object);
         oos.flush();
     }
-    
+
+    /**
+     * Receives an object from the provided socket's input stream.
+     *
+     * @param socket The socket used for communication.
+     * @return The received object.
+     * @throws IOException If an I/O error occurs during the receiving process.
+     * @throws ClassNotFoundException If the class of the received object cannot
+     * be found.
+     */
     public Object receive(Socket socket) throws IOException, ClassNotFoundException {
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         return ois.readObject();
--- SeminarsOrganizerCommon/src/main/java/com/fon/common/intercomm/Response.java
+++ SeminarsOrganizerCommon/src/main/java/com/fon/common/intercomm/Response.java
@@ -7,43 +7,106 @@ package com.fon.common.intercomm;
 import java.io.Serializable;
 
 /**
+ * Class representing a response that server sends to a client. It contains
+ * the purpose of the response, data that will be sent and information about errors
+ * that might have happened.
  *
  * @author Aleksa
+ * @since 0.0.1
  */
 public class Response implements Serializable {
+
+    /**
+     * Data server sends (server receives) as {@code Object}.
+     */
     private Object result;
+
+    /**
+     * Operation client needs to perform as {@code Operation} when server sends
+     * the response.
+     */
     private Operation operation;
+
+    /**
+     * Error that might have happened at server's side as {@code Exception}.
+     */
     private Exception exception;
 
+    /**
+     * Constructor with all parameters.
+     *
+     * @param result Data server sends (server receives) as {@code Object}.
+     * @param operation Operation client needs to perform as {@code Operation}
+     * when server sends the response.
+     * @param exception Error that might have happened at server's side as
+     * {@code Exception}.
+     */
     public Response(Object result, Operation operation, Exception exception) {
         this.result = result;
         this.operation = operation;
         this.exception = exception;
     }
 
+    /**
+     * Non-parametric constructor.
+     */
     public Response() {
     }
 
+    /**
+     * Getter for result.
+     *
+     * @return Data server sends (server receives) as {@code Object}.
+     */
     public Object getResult() {
         return result;
     }
 
+    /**
+     * Setter for result.
+     *
+     * @param result Data server sends (server receives) as {@code Object}.
+     */
     public void setResult(Object result) {
         this.result = result;
     }
 
+    /**
+     * Getter for operation.
+     *
+     * @return Operation client needs to perform as {@code Operation} when
+     * server sends the response.
+     */
     public Operation getOperation() {
         return operation;
     }
 
+    /**
+     * Setter for operation.
+     *
+     * @param operation Operation client needs to perform as {@code Operation}
+     * when server sends the response.
+     */
     public void setOperation(Operation operation) {
         this.operation = operation;
     }
 
+    /**
+     * Getter for exception.
+     *
+     * @return Error that might have happened at server 's side as
+     * {@code Exception}.
+     */
     public Exception getException() {
         return exception;
     }
 
+    /**
+     * Setter for exception.
+     *
+     * @param exception Error that might have happened at server 's side as
+     * {@code Exception}.
+     */
     public void setException(Exception exception) {
         this.exception = exception;
     }
