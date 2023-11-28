899f2ae6d53ac6917f1def409e57c1ba70479afe
aleksa-radojicic <aleksa.radojicic@gmail.com>
2023-11-28 22:33:39 +0100

    Added javadoc to classes in exceptions package

--- SeminarsOrganizerCommon/src/main/java/com/fon/common/exceptions/ServerValidationException.java
+++ SeminarsOrganizerCommon/src/main/java/com/fon/common/exceptions/ServerValidationException.java
@@ -5,14 +5,35 @@
 package com.fon.common.exceptions;
 
 /**
+ * Exception indicating validation failure for certain server-related
+ * operations.
+ *
+ * <p>
+ * It is thrown only in this situations:
+ * </p>
+ *
+ * <ul>
+ * <li>Admin is already logged in;</li>
+ * <li>Admin tried to log in with wrong credentials.</li>
+ * </ul>
  *
  * @author Aleksa
+ * @since 0.0.1
  */
 public class ServerValidationException extends Exception {
 
+    /**
+     * Non-parametric constructor.
+     */
     public ServerValidationException() {
     }
 
+    /**
+     * Constructor with message parameter.
+     *
+     * @param message The detail message describing the validation failure or
+     * error condition.
+     */
     public ServerValidationException(String message) {
         super(message);
     }
