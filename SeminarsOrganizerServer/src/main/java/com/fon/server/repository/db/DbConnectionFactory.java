ed9402ff9153cfdb47c48bfe95d0915bca718f84
aleksa-radojicic <aleksa.radojicic@gmail.com>
2023-11-30 16:10:17 +0100

    Added javadoc to classes in repository package

--- SeminarsOrganizerServer/src/main/java/com/fon/server/repository/db/DbConnectionFactory.java
+++ SeminarsOrganizerServer/src/main/java/com/fon/server/repository/db/DbConnectionFactory.java
@@ -14,17 +14,33 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 /**
+ * Singleton class for connecting to the database.
  *
  * @author Aleksa
  */
 public class DbConnectionFactory {
 
-    private Connection connection;
+    /**
+     * The singleton instance of the {@code DbConnectionFactory} class.
+     */
     private static DbConnectionFactory instance;
 
+    /**
+     * SQL connection instance as {@code Connection}.
+     */
+    private Connection connection;
+
+    /**
+     * Private non-parametric constructor.
+     */
     private DbConnectionFactory() {
     }
 
+    /**
+     * Getter of instance (Singleton pattern).
+     *
+     * @return The singleton instance of the {@code DbConnectionFactory} class.
+     */
     public static DbConnectionFactory getInstance() {
         if (instance == null) {
             instance = new DbConnectionFactory();
@@ -32,6 +48,17 @@ public class DbConnectionFactory {
         return instance;
     }
 
+    /**
+     * Connects to a database from the {@code dbconfig} properties file.
+     *
+     * @return SQL connection instance as {@code Connection}.
+     * @throws java.io.FileNotFoundException When there is no {@code dbconfig}
+     * properties file.
+     * @throws java.io.IOException When data from {@code dbconfig} properties
+     * file cannot be properly read.
+     * @throws java.sql.SQLException When database server is not up or the
+     * database credentials from {@code dbconfig} properties file are incorrect.
+     */
     public Connection getConnection() throws FileNotFoundException, IOException, SQLException {
         if (connection == null || connection.isClosed()) {
             Properties properties = new Properties();
--- SeminarsOrganizerServer/src/main/java/com/fon/server/repository/db/DbRepository.java
+++ SeminarsOrganizerServer/src/main/java/com/fon/server/repository/db/DbRepository.java
@@ -8,24 +8,51 @@ package com.fon.server.repository.db;
 import com.fon.server.repository.Repository;
 
 /**
+ * Parametric interface with default implementations of connect, disconnect, commit and
+ * rollback operations regarding a database.
  *
  * @author Aleksa
+ * @since 0.0.1
  * @param <T>
  */
 public interface DbRepository<T> extends Repository<T> {
 
+    /**
+     * Connects to a database.
+     *
+     * @throws Exception When the connection with the database cannot be
+     * established.
+     */
     default void connect() throws Exception {
         DbConnectionFactory.getInstance().getConnection();
     }
 
+    /**
+     * Disconnects from the database.
+     *
+     * @throws Exception When the connection to the database cannot be
+     * closed.
+     */
     default void disconnect() throws Exception {
         DbConnectionFactory.getInstance().getConnection().close();
     }
 
+    /**
+     * Commits a database transaction.
+     *
+     * @throws Exception When an error happened during the committing of the
+     * transaction.
+     */
     default void commit() throws Exception {
         DbConnectionFactory.getInstance().getConnection().commit();
     }
 
+    /**
+     * Rolls back a database transaction.
+     *
+     * @throws Exception When an error happened during the rolling back of the
+     * transaction.
+     */
     default void rollback() throws Exception {
         DbConnectionFactory.getInstance().getConnection().rollback();
     }
